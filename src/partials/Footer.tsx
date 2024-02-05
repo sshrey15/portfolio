b454456157f96882c3572ef7245ac2660a453c68
shrey <shreysingh152004@gmail.com>
2024-02-05 22:17:39 +0530

    Update copyright year and format in Footer component

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -3,8 +3,7 @@ import { Section } from 'astro-boilerplate-components';
 const Footer = () => (
   <Section>
     <div className="flex flex-col items-center justify-center gap-4">
-      <div>Copyright 2024</div>
-      <div>Thank you for visiting our website. We appreciate your support!</div>
+      <div>© Copyright 2024 </div>
     </div>
   </Section>
 );
