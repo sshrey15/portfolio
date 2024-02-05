10bc3f8f245cc435b684eedcf31b29195f287a19
shrey <shreysingh152004@gmail.com>
2024-02-05 22:16:00 +0530

    Update footer content

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -2,7 +2,10 @@ import { Section } from 'astro-boilerplate-components';
 
 const Footer = () => (
   <Section>
-    <div className="flex items-center justify-center gap-4">shrey</div>
+    <div className="flex flex-col items-center justify-center gap-4">
+      <div>Copyright 2024</div>
+      <div>Thank you for visiting our website. We appreciate your support!</div>
+    </div>
   </Section>
 );
 
