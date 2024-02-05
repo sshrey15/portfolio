1308a07534eb6c8b1926aa792a9102cdb06eaed8
shrey <shreysingh152004@gmail.com>
2024-02-05 22:14:06 +0530

    Remove copyright information and update footer content

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -1,10 +1,8 @@
-import { FooterCopyright, Section } from 'astro-boilerplate-components';
-
-import { AppConfig } from '@/utils/AppConfig';
+import { Section } from 'astro-boilerplate-components';
 
 const Footer = () => (
   <Section>
-    <FooterCopyright site_name={AppConfig.site_name} />
+    <div className="flex items-center justify-center gap-4">shrey</div>
   </Section>
 );
 
